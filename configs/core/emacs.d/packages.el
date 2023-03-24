da5aaf3d040c46c3af101df2aed5716a406b7c0f
Michael Bridgen <mikeb@squaremobius.net>
2023-03-24 11:05:34 +0000

    Switch emacs package managers around
    Signed-off-by: Michael Bridgen <mikeb@squaremobius.net>

--- configs/core/emacs.d/packages.el
+++ configs/core/emacs.d/packages.el
@@ -1,9 +1,11 @@
 ;; Marmalade package repo
 (require 'package)
+; Marmalade seems to have been unavailable since about 2021:
+; https://www.emacswiki.org/emacs/MarmaladeRepo
+;(add-to-list 'package-archives
+;             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
